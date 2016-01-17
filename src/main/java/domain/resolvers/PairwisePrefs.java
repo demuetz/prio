e930b812b7780cb9537c6082ee9bf4a303e536e2
Wario Mettbach <demuetz@gmx.net>
2016-01-18 00:32:29 +0100

    refactoring SchulzeMethodResolver

---
+++ src/main/java/domain/resolvers/PairwisePrefs.java
@@ -0,0 +1,109 @@
+package domain.resolvers;
+
+import domain.votes.Vote;
+import domain.votes.Votes;
+import javafx.util.Pair;
+import org.jgrapht.graph.SimpleDirectedWeightedGraph;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class PairwisePrefs {
+    private final SimpleDirectedWeightedGraph<Integer, PrioWeightedEdge> prefsGraph;
+
+    public static PairwisePrefs from(Votes votes) {
+        SimpleDirectedWeightedGraph<Integer, PrioWeightedEdge> prefsGraph =
+                new SimpleDirectedWeightedGraph<>(PrioWeightedEdge.class);
+
+        for (Vote v : votes){
+
+            int currentOptionIndex = 0;
+
+            int[] ranking = v.getRanking();
+
+            addNonExistingVertices(prefsGraph, ranking);
+
+            for (int winner : ranking){
+
+                prefsGraph.addVertex(winner);
+
+                for (int i = currentOptionIndex+1; i < ranking.length; i++) {
+
+                    int loser = ranking[i];
+
+                    if (!prefsGraph.containsEdge(winner, loser)) {
+                        prefsGraph.addEdge(winner, loser);
+                    } else {
+                        incrementEdgeWeight(prefsGraph, winner, loser);
+                    }
+                }
+                currentOptionIndex++;
+            }
+        }
+
+        return new PairwisePrefs(prefsGraph);
+    }
+
+    private static void addNonExistingVertices(SimpleDirectedWeightedGraph<Integer, PrioWeightedEdge> prefsGraph, int[] ranking) {
+        for (int option :ranking){
+            prefsGraph.addVertex(option);
+        }
+    }
+
+    private static void incrementEdgeWeight(SimpleDirectedWeightedGraph<Integer, PrioWeightedEdge> prefsGraph, int winner, int loser) {
+        PrioWeightedEdge e = prefsGraph.getEdge(winner, loser);
+        prefsGraph.setEdgeWeight(e, prefsGraph.getEdgeWeight(e) + 1.0);
+    }
+
+    private PairwisePrefs(SimpleDirectedWeightedGraph<Integer, PrioWeightedEdge> prefsGraph) {
+
+        this.prefsGraph = prefsGraph;
+    }
+
+    private int weightOf(int winner, int loser) {
+        PrioWeightedEdge edge = prefsGraph.getEdge(winner, loser);
+
+        if (edge == null) return 0;
+
+        return (int) prefsGraph.getEdgeWeight(edge);
+    }
+
+    public StrongestPathWeights calculateStrongestPathWeights() {
+        Set<Integer> options = prefsGraph.vertexSet();
+
+        Map<Pair<Integer, Integer>, Integer> strongestPathWeights = new HashMap<>();
+
+        for (int option1 : options){
+            for (int option2 : options){
+                if (option1 != option2){
+                    Pair<Integer, Integer> pair = new Pair<>(option1, option2);
+
+                    int e1_2 = weightOf(option1, option2);
+                    int e2_1 = weightOf(option2, option1);
+
+                    int w = e1_2 > e2_1 ? e1_2 : 0;
+
+                    strongestPathWeights.put(pair, w);
+                }
+            }
+        }
+
+        for (int i : options){
+            for (int j : options){
+                if (i != j){
+                    options.stream().filter(k -> i != k && j != k).forEach(k -> {
+                        Pair<Integer, Integer> pair = new Pair<>(j, k);
+                        int jk = strongestPathWeights.get(pair);
+                        int ji = strongestPathWeights.get(new Pair<>(j, i));
+                        int ik = strongestPathWeights.get(new Pair<>(i, k));
+
+                        int strength = Math.max(jk, Math.min(ji, ik));
+                        strongestPathWeights.put(pair, strength);
+                    });
+                }
+            }
+        }
+        return new StrongestPathWeights(strongestPathWeights);
+    }
+}
--- src/main/java/domain/resolvers/SchulzeMethodResolver.java
+++ src/main/java/domain/resolvers/SchulzeMethodResolver.java
@@ -3,12 +3,11 @@ package domain.resolvers;
 import domain.sessions.PrioItems;
 import domain.sessions.PrioResolver;
 import domain.sessions.PrioResult;
-import domain.votes.Vote;
 import domain.votes.Votes;
-import javafx.util.Pair;
-import org.jgrapht.graph.SimpleDirectedWeightedGraph;
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class SchulzeMethodResolver implements PrioResolver {
@@ -16,11 +15,10 @@ public class SchulzeMethodResolver implements PrioResolver {
     @Override
     public PrioResult resolve(PrioItems options, Votes votes) {
 
-        SimpleDirectedWeightedGraph<Integer, PrioWeightedEdge> pwpGraph = calculatePairwisePrefs(options, votes);
-
-        StrongestPathWeights strongestPathWeights = calculateStrongestPathWeights(options, pwpGraph);
-
-        Map<Integer, Integer> scores = strongestPathWeights.calculateScores();
+        Map<Integer, Integer> scores =
+                PairwisePrefs.from(votes)
+                        .calculateStrongestPathWeights()
+                        .calculateScores();
 
         return new PrioResult(toArray(toRankedIds(scores)));
     }
@@ -40,91 +38,5 @@ public class SchulzeMethodResolver implements PrioResolver {
             i++;
         }
         return ranked;
-    }
-
-    private StrongestPathWeights calculateStrongestPathWeights(PrioItems options, SimpleDirectedWeightedGraph<Integer, PrioWeightedEdge> pwpGraph) {
-        Map<Pair<Integer, Integer>, Integer> strongestPathWeights = new HashMap<>();
-
-        for (int winner : options.getIds()){
-            for (int loser : options.getIds()){
-                if (winner != loser){
-                    Pair<Integer, Integer> pair = new Pair<>(winner, loser);
-
-                    int e1_2 = weight(pwpGraph, winner, loser);
-                    int e2_1 = weight(pwpGraph, loser, winner);
-
-                    int w = e1_2 > e2_1 ? e1_2 : 0;
-
-                    strongestPathWeights.put(pair, w);
-                }
-            }
-        }
-
-        for (int i : options.getIds()){
-            for (int j : options.getIds()){
-                if (i != j){
-                    for (int k : options.getIds()){
-                        if (i != k && j != k){
-                            Pair<Integer, Integer> pair = new Pair<>(j,k);
-                            int jk = strongestPathWeights.get(pair);
-                            int ji = strongestPathWeights.get(new Pair<>(j,i));
-                            int ik = strongestPathWeights.get(new Pair<>(i,k));
-
-                            int strength = Math.max(jk, Math.min(ji, ik));
-                            strongestPathWeights.put(pair, strength);
-                        }
-                    }
-                }
-            }
-        }
-        return new StrongestPathWeights(strongestPathWeights);
-    }
-
-    private int weight(SimpleDirectedWeightedGraph<Integer, PrioWeightedEdge> pwpGraph, int winner, int loser) {
-
-        PrioWeightedEdge edge = pwpGraph.getEdge(winner, loser);
-
-        if (edge == null) return 0;
-
-        return (int) pwpGraph.getEdgeWeight(edge);
-    }
-
-    private SimpleDirectedWeightedGraph<Integer, PrioWeightedEdge> calculatePairwisePrefs(PrioItems options, Votes votes) {
-
-        SimpleDirectedWeightedGraph<Integer, PrioWeightedEdge> prefsGraph =
-                new SimpleDirectedWeightedGraph<>(PrioWeightedEdge.class);
-
-        for (int optionId : options.getIds()){
-            prefsGraph.addVertex(optionId);
-        }
-
-        for (Vote v : votes){
-
-            int currentOptionIndex = 0;
-
-            int[] ranking = v.getRanking();
-
-            for (int winner : ranking){
-
-                for (int i = currentOptionIndex+1; i < ranking.length; i++) {
-
-                    int loser = ranking[i];
-
-                    if (!prefsGraph.containsEdge(winner, loser)) {
-                        prefsGraph.addEdge(winner, loser);
-                    } else {
-                        incrementEdgeWeight(prefsGraph, winner, loser);
-                    }
-                }
-                currentOptionIndex++;
-            }
-        }
-
-        return prefsGraph;
-    }
-
-    private void incrementEdgeWeight(SimpleDirectedWeightedGraph<Integer, PrioWeightedEdge> prefsGraph, int winner, int loser) {
-        PrioWeightedEdge e = prefsGraph.getEdge(winner, loser);
-        prefsGraph.setEdgeWeight(e, prefsGraph.getEdgeWeight(e) + 1.0);
     }
 }
